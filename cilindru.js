90680dc9a4e0f6009d440c5ccbd997c372c30345
Fota Marian <75865214+spyritzone@users.noreply.github.com>
2021-04-04 00:00:34 +0300

    Add files via upload

---
+++ cilindru.js
@@ -0,0 +1,6 @@
+var π = 3.14;
+var π2 = π ** 2;
+var numberRadius = prompt ("Please enter the radius of the base of the cylinder");
+var numberHeight = prompt ("Please enter the height");
+var sum = parseInt (numberRadius) * parseInt(numberHeight) * π2;
+alert("Sum is: " + sum); 
